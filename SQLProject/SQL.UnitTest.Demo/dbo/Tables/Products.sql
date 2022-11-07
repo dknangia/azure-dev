720fdc0e02c562a63511ac7e0216be664653d36e
Dknangia <dknangia@gmail.com>
2022-11-07 17:00:43 -0500

    Removing and adding new project

--- SQLProject/SQL.UnitTest.Demo/dbo/Tables/Products.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Products] (
-    [ProductId]   INT             IDENTITY (1, 1) NOT NULL,
-    [Name]        NVARCHAR (MAX)  NULL,
-    [Description] NVARCHAR (MAX)  NULL,
-    [Price]       DECIMAL (18, 2) NOT NULL,
-    [Category]    NVARCHAR (MAX)  NULL,
-    CONSTRAINT [PK_dbo.Products] PRIMARY KEY CLUSTERED ([ProductId] ASC)
-);
-
