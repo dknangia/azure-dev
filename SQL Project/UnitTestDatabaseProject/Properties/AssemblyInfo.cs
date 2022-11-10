db2c97f42153a6c5375946933e1b097af9d8a617
Dknangia <dknangia@gmail.com>
2022-11-09 22:05:04 -0500

    Properties.

---
+++ SQL Project/UnitTestDatabaseProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestDatabaseProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TRADER Corporation")]
+[assembly: AssemblyProduct("UnitTestDatabaseProject")]
+[assembly: AssemblyCopyright("Copyright © TRADER Corporation 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c21196fe-cc40-420c-8b5f-ad61bb817cbb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
