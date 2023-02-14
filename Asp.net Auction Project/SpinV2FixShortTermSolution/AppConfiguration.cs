0436065eba41dc7085f96b3321bae2bfeceddafe
Dknangia <dknangia@gmail.com>
2023-02-13 20:15:44 -0500

    App configuration file

---
+++ Asp.net Auction Project/SpinV2FixShortTermSolution/AppConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace SpinV2FixShortTermSolution
+{
+
+    public class AppConfiguration : IAppConfiguration
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppConfiguration()
+        {
+
+
+
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var appConfig = _configuration.GetSection(nameof(AppConfig)).Get<AppConfig>();
+
+
+
+        }
+
+        public string GetValue(string settingName)
+        {
+            return _configuration[settingName] ?? string.Empty;
+        }
+
+    }
+
+    public class AppConfig
+    {
+        public string? Environment { get; set; }
+    }
+
+    public interface IAppConfiguration
+    {
+        string GetValue(string settingName);
+    }
+
+}
