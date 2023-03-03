384692ee6b6319613ef63006f89f5c5946a36d50
Dknangia <dknangia@gmail.com>
2023-03-03 15:11:55 -0500

    Location class for special bindings

---
+++ Web Api Project/Chapter02/Location.cs
@@ -0,0 +1,29 @@
+using System.Globalization;
+
+public class Location
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+
+    public static bool TryParse(string? value, IFormatProvider? provider, out Location? location)
+    {
+        if (!string.IsNullOrEmpty(value))
+        {
+            var values = value.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            if (values.Length == 2
+                && double.TryParse(values[0], NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out var latitude)
+                && double.TryParse(values[1], NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out var longitude))
+            {
+                location = new Location
+                {
+                    Latitude = latitude,
+                    Longitude = longitude
+                };
+
+                return true;
+            }
+        }
+        location = null;
+        return false;
+    }
+}
