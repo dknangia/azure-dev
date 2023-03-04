5b7ffbfc80f8e48c2cfe8a9d914119fb89b9549b
Dknangia <dknangia@gmail.com>
2023-03-04 18:26:33 -0500

    IEndPointRouteBuilderExtension

---
+++ Web Api Project/Chapter02/IEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace Chapter02;
+
+public static class EndPointRouteBuilderExtensions
+{
+    public static void MapEndpoints(this IEndpointRouteBuilder app, Assembly assembly)
+    {
+        var endpointRouteHandlerInterfaceType =
+            typeof(IEndpointRouteHandler);
+        var endpointRouteHandlerTypes =
+            assembly.GetTypes().Where(t =>
+                t is {IsClass: true, IsAbstract: false, IsGenericType: false} 
+                && t.GetConstructor(Type.EmptyTypes) != null 
+                && endpointRouteHandlerInterfaceType
+                    .IsAssignableFrom(t));
+        foreach (var endpointRouteHandlerType in
+                 endpointRouteHandlerTypes)
+        {
+            var instantiatedType = (IEndpointRouteHandler)
+                Activator.CreateInstance
+                    (endpointRouteHandlerType)!;
+            instantiatedType.MapEndpoints(app);
+        }
+    }
+}
