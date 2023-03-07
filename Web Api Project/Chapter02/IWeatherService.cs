2a7a98822ffed4491e3f7f03ad140c78979c31e1
Dknangia <dknangia@gmail.com>
2023-03-07 18:50:29 -0500

    Changes in project file

---
+++ Web Api Project/Chapter02/IWeatherService.cs
@@ -0,0 +1,27 @@
+namespace Chapter02;
+
+public interface IWeatherService
+{
+    WeatherForecast[] GetForecast(); 
+
+}
+
+public class WeatherService : IWeatherService
+{
+    public WeatherForecast[] GetForecast()
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateTime.Now.AddDays(index),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    }
+}
