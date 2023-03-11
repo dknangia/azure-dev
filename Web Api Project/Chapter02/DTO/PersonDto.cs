6d229b12d9ca862c0a553594fbd494841350afe6
Dknangia <dknangia@gmail.com>
2023-03-10 19:20:17 -0500

    DTO Class

---
+++ Web Api Project/Chapter02/DTO/PersonDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chapter02.DTO
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
