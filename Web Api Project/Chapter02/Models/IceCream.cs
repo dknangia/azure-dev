d144fb296d3a196b23a458ffe36aff7ca010033e
Dknangia <dknangia@gmail.com>
2023-03-12 14:43:40 -0400

    Ice cream model class

---
+++ Web Api Project/Chapter02/Models/IceCream.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter02.Models
+{
+    public class IceCream
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class IceCreamDb : DbContext
+    {
+        public IceCreamDb(DbContextOptions options ):base(options)
+        {
+            
+        }
+    }
+}
+
