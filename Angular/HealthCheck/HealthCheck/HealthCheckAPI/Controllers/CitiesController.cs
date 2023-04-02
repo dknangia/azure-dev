2623e7076d6294da71c6b10c8ae1203b5ca30b58
Dknangia <dknangia@gmail.com>
2023-04-02 15:30:20 -0400

    Adding an action method

---
+++ Angular/HealthCheck/HealthCheck/HealthCheckAPI/Controllers/CitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCheckAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+
+        
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>
+            {
+                new {id =1 , Name = "New York"},
+                new {id =2, Name = "Canada"}
+            });
+        }
+
+
+        [HttpGet("getCity")]
+        public JsonResult GetCity()
+        {
+            return new JsonResult(new List<object>
+            {
+                new {id =1 , Name = "New York"},
+               
+            });
+        }
+    }
+}
