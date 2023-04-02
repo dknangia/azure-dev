e9e0ab25b4269a89a444cf4064137d870042f4fe
Dknangia <dknangia@gmail.com>
2023-04-02 15:44:53 -0400

    Data source

---
+++ Angular/HealthCheck/HealthCheck/HealthCheckAPI/CityDataStore.cs
@@ -0,0 +1,43 @@
+﻿using HealthCheckAPI.Models;
+
+namespace HealthCheckAPI
+{
+    public class CitiesDataStores
+    {
+        public List<CityDto> Cities { get; set; }
+        public static CitiesDataStores Current { get; } = new CitiesDataStores(); 
+        public CitiesDataStores()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New york city",
+                    Description = "The one with big park"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "New Delhi",
+                    Description = "The one with tallest building"
+                },
+
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Waterloo",
+                    Description = "The one with a big Park and colleges"
+                },
+
+                new CityDto()
+                {
+                    Id = 4,
+                    Name = "Toronto",
+                    Description = "The city with TV tower"
+                },
+
+            };
+        }
+    }
+}
