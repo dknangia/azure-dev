e9f946360a783c933813ae86a39ae03d2774e828
Dknangia <dknangia@gmail.com>
2023-04-10 22:24:07 -0400

    Changes in app module constructor

--- Angular/HealthCheck/HealthCheck/HealthCheck/src/app/app.module.ts
+++ Angular/HealthCheck/HealthCheck/HealthCheck/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import {environment} from '../../Environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,4 +16,11 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor() {
+    if (environment.production) {
+
+    }
+  }
+}
