9383f5e16876ab0d0696205cb295e7cfbbacb68e
Dknangia <dknangia@gmail.com>
2023-04-12 21:23:05 -0400

    Point of interest file.

--- Angular/HealthCheck/HealthCheck/HealthCheckAPI/Models/PointOfInterest.cs
+++ Angular/HealthCheck/HealthCheck/HealthCheckAPI/Models/PointOfInterest.cs
@@ -1,14 +1,26 @@
-﻿namespace HealthCheckAPI.Models;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-public class PointOfInterest
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-}
+namespace HealthCheckAPI.Models;
+
+//public class PointOfInterest
+//{
+//    public int Id { get; set; }
+//    public string? Name { get; set; }
+//    public string? Description { get; set; }
+//}
 
 public class PointOfInterestCreationDto
 {
-    public string? Name { get; set; }
+    
+    public string? Name { get; set; } = string.Empty;
     public string? Description { get; set; }
+
+}
+
+public class PointOfInterestDto
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+
 }
