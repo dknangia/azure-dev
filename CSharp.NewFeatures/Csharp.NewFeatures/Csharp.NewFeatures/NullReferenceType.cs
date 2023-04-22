c8655bec65f5fd87ce55aeee54ba594449b52203
Dknangia <dknangia@gmail.com>
2023-04-22 15:17:48 -0400

    Nullable reference types

--- CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/NullReferenceType.cs
+++ CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/NullReferenceType.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,24 +14,38 @@ namespace Csharp.NewFeatures
         {
             
         }
+
+        public void PrintBlogPosts(BlogPost post)
+        {
+            if (post?.Comments == null) return;
+
+
+
+            foreach (var comment in post.Comments)
+            {
+                var commentPreview = comment?.Body is {Length: > 10}
+                    ? $"{comment.Body[..10]}..."
+                    : comment.Body;
+            }
+        }
     }
 
 
     public class BlogPost
     {
-        public string Title { get; set; }
-        public List<Comment> Comments{ get; set; }
+        public string? Title { get; set; }
+        public List<Comment>? Comments { get; } = new List<Comment>();
     }
 
     public class Comment
     {
-        public string Body { get; set; }
-        public string PostedBy { get; set; }
+        public string? Body { get; set; }
+        public string? PostedBy { get; set; }
     }
 
     public class Author
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
     }
 }
--- CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/Program.cs
+++ CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/Program.cs
@@ -1,1 +1,1 @@
-﻿
+﻿Console.Write("Nullable Reference");
