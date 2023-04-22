72019a3e1625cb6834ecd1fa78265eb988ee44d6
Dknangia <dknangia@gmail.com>
2023-04-22 14:48:17 -0400

    Null reference class

---
+++ CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/NullReferenceType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.NewFeatures
+{
+    public class NullReferenceType
+    {
+        public NullReferenceType()
+        {
+            
+        }
+    }
+
+
+    public class BlogPost
+    {
+        public string Title { get; set; }
+        public List<Comment> Comments{ get; set; }
+    }
+
+    public class Comment
+    {
+        public string Body { get; set; }
+        public string PostedBy { get; set; }
+    }
+
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
