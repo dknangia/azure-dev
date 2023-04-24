452c27a6d6562377ec65d873aa702c00f2575766
Dknangia <dknangia@gmail.com>
2023-04-24 09:42:37 -0400

    Refactor of the code

---
+++ CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/8.0/IndicesAndRanges.cs
@@ -0,0 +1,29 @@
+﻿namespace Csharp.NewFeatures._8._0
+{
+    public static class IndicesAndRanges
+    {
+        public static void Run()
+        {
+            var numbers = Enumerable.Range(0, 10).ToArray();
+
+            Console.WriteLine("Complete array");
+            var copy = numbers[0..]; // All items int the array 
+            Console.WriteLine(string.Join("|", copy));
+
+            Console.WriteLine("All but first item is not included");
+            var allButFirst = numbers[1..]; // Exclude the first item of the array e.g. 0
+            Console.WriteLine(string.Join("|", allButFirst));
+
+
+            Console.WriteLine("Last item of the range");
+            var lastItemRange = numbers[^2];
+            Console.WriteLine(string.Join("|", lastItemRange));
+
+
+            Console.WriteLine("Custom range 4..^3 e.g. Include from 5the item to next three items");
+            var customrange = numbers[4..^3];
+            Console.WriteLine(string.Join("|", customrange));
+
+        }
+    }
+}
---
+++ CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/8.0/NullReferenceType.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.NewFeatures
+{
+    public class NullReferenceType
+    {
+        public NullReferenceType()
+        {
+            
+        }
+
+        public void PrintBlogPosts(BlogPost post)
+        {
+            if (post?.Comments == null) return;
+
+
+
+            foreach (var comment in post.Comments)
+            {
+                var commentPreview = comment?.Body is {Length: > 10}
+                    ? $"{comment.Body[..10]}..."
+                    : comment?.Body;
+            }
+        }
+    }
+
+
+    public class BlogPost
+    {
+        public string? Title { get; set; }
+        public List<Comment>? Comments { get; } = new List<Comment>();
+    }
+
+    public class Comment
+    {
+        public string? Body { get; set; }
+        public string? PostedBy { get; set; }
+    }
+
+    public class Author
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
