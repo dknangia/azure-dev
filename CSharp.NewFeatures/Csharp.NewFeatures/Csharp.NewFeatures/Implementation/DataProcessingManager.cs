efe83f995ee21ae269402ba0d926aa46fe5e7349
Dknangia <dknangia@gmail.com>
2023-04-29 14:26:21 -0400

    Logic in data processing class

--- CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/Implementation/DataProcessingManager.cs
+++ CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/Implementation/DataProcessingManager.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
+using Csharp.NewFeatures.Implementation.Models;
 
 namespace Csharp.NewFeatures.Implementation
 {
     public static class DataProcessingManager
     {
-        static readonly double Latitude = 43.466667;
-        static readonly double Longitude = -80.516670;
+        private const double Latitude = 43.466667;
+        private const double Longitude = -80.516670;
+
         public static void DataProcessing()
         {
-            var companies = DataSource.GetData();
+            var companies = DataSource.GetData().Map((Latitude, Longitude));
 
             var companyGrouped = companies.GroupBy(x => x.DealerGroupId);
 
@@ -23,7 +20,7 @@ namespace Csharp.NewFeatures.Implementation
                 foreach (var company in item)
                 {
                     Console.WriteLine($"\t -{company.Name}");
-                    Console.WriteLine($"\t - {CalculateDistance(company.Latitude, company.Longitude)} Km");
+                    Console.WriteLine($"\t\t -{company.Distance} Km");
                 }
             }
 
@@ -32,16 +29,42 @@ namespace Csharp.NewFeatures.Implementation
         public static int CalculateDistance(double dealerLatitude, double dealerLongitude)
         {
 
-            if (double.IsNaN(Latitude) || double.IsNaN(Longitude) || double.IsNaN(dealerLatitude) || double.IsNaN(dealerLongitude))
+            if (double.IsNaN(Latitude) || double.IsNaN(Longitude) || double.IsNaN(dealerLatitude) ||
+                double.IsNaN(dealerLongitude))
                 throw new ArgumentException("Latitude or Longitude are not in correct format");
             double x1 = Latitude * (Math.PI / 180.0);
             double y1 = Longitude * (Math.PI / 180.0);
             double x2 = dealerLatitude * (Math.PI / 180.0);
             double y2 = dealerLongitude * (Math.PI / 180.0) - y1;
-            double d3 = Math.Pow(Math.Sin((x2 - x1) / 2.0), 2.0) + Math.Cos(x1) * Math.Cos(x2) * Math.Pow(Math.Sin(y2 / 2.0), 2.0);
+            double d3 = Math.Pow(Math.Sin((x2 - x1) / 2.0), 2.0) +
+                        Math.Cos(x1) * Math.Cos(x2) * Math.Pow(Math.Sin(y2 / 2.0), 2.0);
             return (int)(6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3))) / 1000);
 
 
         }
     }
+
+    public static class Mapper
+    {
+        public static List<Company> Map(this List<Company>? companies, (double latitdue, double longitude) location)
+        {
+            var data = new List<Company>();
+
+            if (companies != null && companies.Any())
+            {
+               data.AddRange( companies.Select(x => new Company
+               {
+                   Name = x.Name,
+                   DealerGroupId = x.DealerGroupId,
+                   Id = x.DealerGroupId,
+                   Distance = DataProcessingManager.CalculateDistance(x.Latitude, x.Longitude)
+
+               }));
+            }
+
+            return data;
+        }
+
+    }
 }
+
