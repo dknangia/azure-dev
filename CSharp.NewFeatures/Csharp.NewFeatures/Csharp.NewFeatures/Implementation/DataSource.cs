170fafb26e39bb5079d4e14bad48ad16b1a4e0a0
Dknangia <dknangia@gmail.com>
2023-04-29 13:48:21 -0400

    Data source for the calculations

--- CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/Implementation/DataSource.cs
+++ CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/Implementation/DataSource.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Csharp.NewFeatures.Implementation.Models;
 
 namespace Csharp.NewFeatures.Implementation
 {
     public static class DataSource
     {
+        //43.466667 -80.516670
         public static IEnumerable<Company> GetData()
         {
             return new List<Company>
             {
                 new Company()
                 {
-                    Id = 101, City = "Brampton", DealerGroupId = 1001, Latitude = 51.213890, Longitude = -102.462776, Name = "Brampton dealers-1001"
+                    Id = 101, City = "Brampton", DealerGroupId = 1001, Latitude = 43.4844178, Longitude = -80.5327317, Name = "Brampton dealers-1001"
                 },
                 new Company()
                 {
@@ -22,49 +24,38 @@ namespace Csharp.NewFeatures.Implementation
                 },
                 new Company()
                 {
-                    Id = 103, City = "Brampton", DealerGroupId = 1002, Latitude = 0, Longitude = 101, Name = "Brampton dealers-1002"
+                    Id = 103, City = "Brampton", DealerGroupId = 1002, Latitude = 50.288055, Longitude = -107.793892, Name = "Brampton dealers-1002"
                 },
                 new Company()
                 {
-                    Id = 104, City = "Brampton", DealerGroupId = 1002, Latitude = 0, Longitude = 101, Name = "Brampton dealers-1002"
+                    Id = 104, City = "Brampton", DealerGroupId = 1002, Latitude = 52.757500, Longitude = -108.286110, Name = "Brampton dealers-1002"
                 },
                 new Company()
                 {
-                    Id = 105, City = "Brampton", DealerGroupId = 1003, Latitude = 0, Longitude = 101, Name = "Brampton dealers-1003"
+                    Id = 105, City = "Brampton", DealerGroupId = 1003, Latitude = 50.393333, Longitude = -105.551941, Name = "Brampton dealers-1003"
                 },
                 new Company()
                 {
-                    Id = 106, City = "Brampton", DealerGroupId = 1003, Latitude = 0, Longitude = 101, Name = "Brampton dealers-1003"
+                    Id = 106, City = "Brampton", DealerGroupId = 1003, Latitude = 50.930557, Longitude = -102.807777, Name = "Brampton dealers-1003"
                 },
                 new Company()
                 {
-                    Id = 107, City = "Brampton", DealerGroupId = 1003, Latitude = 0, Longitude = 101, Name = "Brampton dealers-1003"
+                    Id = 107, City = "Brampton", DealerGroupId = 1003, Latitude = 52.856388, Longitude = -104.610001, Name = "Brampton dealers-1003"
                 },
                 new Company()
                 {
-                    Id = 108, City = "Brampton", DealerGroupId = 1004, Latitude = 0, Longitude = 101, Name = "Brampton dealers-1004"
+                    Id = 108, City = "Brampton", DealerGroupId = 1004, Latitude = 52.289722, Longitude =  -106.666664, Name = "Brampton dealers-1004"
                 },
                 new Company()
                 {
-                    Id = 109, City = "Brampton", DealerGroupId = 1004, Latitude = 0, Longitude = 101, Name = "Brampton dealers-1004"
+                    Id = 109, City = "Brampton", DealerGroupId = 1004, Latitude = 52.201942, Longitude = -105.123055, Name = "Brampton dealers-1004"
                 },
                 new Company()
                 {
-                    Id = 110, City = "Brampton", DealerGroupId = 1004, Latitude = 0, Longitude = 101, Name = "Brampton dealers-1004"
+                    Id = 110, City = "Brampton", DealerGroupId = 1004, Latitude = 53.278046, Longitude = -110.005470, Name = "Brampton dealers-1004"
                 }
 
             }; 
         }
-    }
-
-    public class Company
-    {
-        public int DealerGroupId { get; set; }
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string City { get; set; } = string.Empty;
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-
     }
 }
