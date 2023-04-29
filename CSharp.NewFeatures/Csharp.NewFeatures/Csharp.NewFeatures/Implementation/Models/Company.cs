b0ad1bd2fad9fd8c10f23af1e4645632f806be73
Dknangia <dknangia@gmail.com>
2023-04-29 13:47:58 -0400

    Company model

---
+++ CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/Implementation/Models/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace Csharp.NewFeatures.Implementation.Models;
+
+public class Company
+{
+    public int DealerGroupId { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+
+}
