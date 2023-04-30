3e2c63ab52a85e8fc5ee176275c4af1da773b383
Dknangia <dknangia@gmail.com>
2023-04-30 14:34:13 -0400

    Trying to implement one line solution for group by

--- CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/Implementation/Models/Company.cs
+++ CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures/Implementation/Models/Company.cs
@@ -1,13 +1,13 @@
 ﻿namespace Csharp.NewFeatures.Implementation.Models;
 
-public class Company
+public record Company
 {
-    public int DealerGroupId { get; set; }
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string City { get; set; } = string.Empty;
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-    public int Distance { get; set; }
+    public int DealerGroupId { get; init; }
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string City { get; init; } = string.Empty;
+    public double Latitude { get; init; }
+    public double Longitude { get; init; }
+    public int Distance { get; init; }
 
 }
