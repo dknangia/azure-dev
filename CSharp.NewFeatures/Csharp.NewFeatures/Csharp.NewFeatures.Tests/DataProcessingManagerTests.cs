a1b945650b18b1dfd95b771d66e71d9fd5478236
Dknangia <dknangia@gmail.com>
2023-05-04 22:13:24 -0400

    DataProcessingManager Test

--- CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures.Tests/DataProcessingManagerTests.cs
+++ CSharp.NewFeatures/Csharp.NewFeatures/Csharp.NewFeatures.Tests/DataProcessingManagerTests.cs
@@ -13,9 +13,7 @@ namespace Csharp.NewFeatures.Tests
         public void CalculateDistance_ThrowsArgumentException_WhenLatitudeIsNaN()
         {
             // Arrange
-            var dealerLatitude = 40.0;
             var dealerLongitude = -75.0;
-
             // Act and Assert
             Assert.Throws<ArgumentException>(() => DataProcessingManager.CalculateDistance(double.NaN, dealerLongitude));
         }
@@ -26,14 +24,9 @@ namespace Csharp.NewFeatures.Tests
             // Arrange
             var dealerLatitude = 40.0;
             var dealerLongitude = -75.0;
-            var expectedDistance = 15; // expected distance in km
+            var expectedDistance = 598; // expected distance in km
 
-            // create a substitute for the Latitude and Longitude values
-            var latitude = Substitute.For<double>();
-            latitude.Returns(37.7749); // set the value of latitude to a known value
-
-            var longitude = Substitute.For<double>();
-            longitude.Returns(-122.4194); // set the value of longitude to a known value
+            
 
             // Act
             var distance = DataProcessingManager.CalculateDistance(dealerLatitude, dealerLongitude);
