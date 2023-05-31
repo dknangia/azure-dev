2ac785c61f81350aa0b9cff54ff0c826991fb9e7
Dknangia <dknangia@gmail.com>
2023-05-31 12:01:51 -0400

    Scully setting file

--- Angular/Chapter-1/my-blog/src/app/articles/articles.component.ts
+++ Angular/Chapter-1/my-blog/src/app/articles/articles.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss'],
 })
-export class ArticlesComponent implements OnInit {
+export class ArticlesComponent implements OnInit, OnDestroy {
   posts: ScullyRoute[] = [];
-
-  constructor(private scullyService: ScullyRoutesService) {}
+  private routeSub: Subscription | undefined;
 
   ngOnInit(): void {
-    this.scullyService.available$.subscribe(
+    this.routeSub = this.scullyService.available$.subscribe(
       (posts) => (this.posts = posts.filter((post) => post.title))
     );
+  }
+
+  constructor(private scullyService: ScullyRoutesService) {}
+  ngOnDestroy(): void {
+    this.routeSub?.unsubscribe;
   }
 }
