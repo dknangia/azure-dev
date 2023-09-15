2e49dd972654905f5db95240ea4021e0a2437da9
Dknangia <dknangia@gmail.com>
2023-09-15 13:32:27 -0400

    New logic populate index

--- ReadJsonDataFromFile/ReadJson/Program.cs
+++ ReadJsonDataFromFile/ReadJson/Program.cs
@@ -1,12 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Text;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
+using ReadJson;
 
-string[] randomLocations =
-{
-    "{\r\n\"type\": \"Point\",\r\n\"coordinates\": [\r\n\t-75.729973,\r\n\t45.3647\r\n\t\t\t\t],\r\n\"crs\": {\r\n\t\"type\": \"name\",\r\n\t\"properties\": {\r\n\t\"name\": \"EPSG:4326\"\r\n\t}",
-    "{\r\n\"type\": \"Point\",\r\n\"coordinates\": [\r\n\t-81.15107,\r\n\t43.01297\r\n\t\t\t\t],\r\n\"crs\": {\r\n\t\"type\": \"name\",\r\n\t\"properties\": {\r\n\t\"name\": \"EPSG:4326\"\r\n\t}",
-    "{\r\n\"type\": \"Point\",\r\n\"coordinates\": [\r\n\t-79.40007,\r\n\t43.708279\r\n\t\t\t\t],\r\n\"crs\": {\r\n\t\"type\": \"name\",\r\n\t\"properties\": {\r\n\t\"name\": \"EPSG:4326\"\r\n\t}"
-};
+
+IAppConfiguration configuration = new AppConfiguration();
+
+AzureManager manager = new(configuration);
+if (manager == null) throw new ArgumentNullException(nameof(manager));
+
+await manager.GetResults();
+
+
+
+
+
+
+
+
