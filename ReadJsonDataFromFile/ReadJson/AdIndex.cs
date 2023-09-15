ccfbaa462a5212c391ea7bdf25fe1c46692aa7b8
Dknangia <dknangia@gmail.com>
2023-09-15 13:32:54 -0400

    Ad Index

---
+++ ReadJsonDataFromFile/ReadJson/AdIndex.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+using Microsoft.Spatial;
+
+namespace ReadJson
+{
+    public class AdIndex
+    {
+
+        public string? Id { get; set; }
+        public string? ForeignId { get; set; }
+        public GeographyPoint? Location { get; set; }
+        public GeographyPoint?[] SearchLocations { get; set; }
+
+
+    }
+
+    //public class GeoLocation
+    //{
+    //    GeographyPoint coordinates { get; set; }
+
+    //}
+}
