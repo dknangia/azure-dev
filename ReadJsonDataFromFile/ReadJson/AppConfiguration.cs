267432b6e905774f24924a7488105b66247f5b81
Dknangia <dknangia@gmail.com>
2023-09-15 13:33:22 -0400

    Configuration

---
+++ ReadJsonDataFromFile/ReadJson/AppConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ReadJson
+{
+    public class AppConfiguration : IAppConfiguration
+    {
+        private readonly IConfiguration _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+
+        public string? GetValue(string key)
+        {
+            return this._configuration[key];
+        }
+    }
+
+    public interface IAppConfiguration
+    {
+        string? GetValue(string key);
+    }
+}
