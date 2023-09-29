e87e2a9d7438e5eb13e9d5c1f268715cc75e86b1
Dknangia <dknangia@gmail.com>
2023-09-29 07:48:03 -0400

    17

---
+++ GeoLocationTestDataRequestAverage/Program.cs
@@ -0,0 +1,9 @@
+﻿using GeoLocationTestDataRequestAverage;
+const int requestCount = 500;
+
+// await new Process().Request(requestCount, "qaads-virtualdealer");
+ await new Process().Request(requestCount, "qaads-control");
+ 
+//new Process().Request(requestCount, "qaads-virtualcontroldealer");
+
+
