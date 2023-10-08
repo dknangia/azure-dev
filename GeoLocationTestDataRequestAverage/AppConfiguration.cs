c2d4f827ad6989c6ea54f31ff55a1aefe14e8ad8
Dknangia <dknangia@gmail.com>
2023-10-08 07:48:02 -0400

    8

---
+++ GeoLocationTestDataRequestAverage/AppConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GeoLocationTestDataRequestAverage
+{
+
+    public class AppConfiguration : IAppConfiguration
+    {
+        private readonly IConfiguration _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+
+        public string? GetValue(string? key)
+        {
+            return this._configuration[key];
+        }
+    }
+
+    public interface IAppConfiguration
+    {
+        string? GetValue(string? key);
+    }
+}
+
