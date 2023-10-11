b707ba92917f146951cfed447c77f25a9b6387f6
Dknangia <dknangia@gmail.com>
2023-10-11 07:48:01 -0400

    5

---
+++ ClientConnectionsTLS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace ClientConnectionsTLS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunProgram();
+        }
+
+        private static void RunProgram()
+        {
+            using (var client = new HttpClient())
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                Console.WriteLine($"{ServicePointManager.SecurityProtocol}");
+                var url = "https://www.howsmyssl.com/a/check";
+                var response = client.GetAsync(url).Result;
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine($"{url} {response.StatusCode} {content}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
