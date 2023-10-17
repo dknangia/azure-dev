49ca90e51dbca681122ab5c174f00e95be6c90fe
Dknangia <dknangia@gmail.com>
2023-10-17 15:52:32 -0400

    to read data from local storage

--- Chrome Extensions/Section 1/popup.js
+++ Chrome Extensions/Section 1/popup.js
@@ -1,7 +1,15 @@
-const timeElement = document.getElementById('time');
+const timeElement = document.getElementById("time");
 const currentTime = new Date().toLocaleTimeString();
 timeElement.textContent = `The current time is ${currentTime}`;
+const nameElement = document.getElementById("name");
 
-chrome.action.setBadgeText({ text: 'TIME' }, () => {
-  console.log('Badge text set');
-}); 
+chrome.action.setBadgeText({ text: "TIME" }, () => {
+  console.log("Badge text set");
+});
+
+
+chrome.storage.sync.get("name", (data) => {
+  if (data.name !== undefined) {
+    nameElement.textContent = `Your name is ${data.name}`;
+  }
+});
