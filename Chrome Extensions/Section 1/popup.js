4f97e741199c7144c3c80bfff0488f453e232767
Deepak Nangia <dknangia@gmail.com>
2024-04-25 20:57:33 -0400

    no message

--- Chrome Extensions/Section 1/popup.js
+++ Chrome Extensions/Section 1/popup.js
@@ -1,11 +1,21 @@
 const timeElement = document.getElementById("time");
+const timerElement = document.getElementById("timer");
 const currentTime = new Date().toLocaleTimeString();
 timeElement.textContent = `The current time is ${currentTime}`;
 const nameElement = document.getElementById("name");
 
-chrome.action.setBadgeText({ text: "TIME" }, () => {
-  console.log("Badge text set");
-});
+function updateTimer(){
+  chrome.storage.local.get(["timer"], function (result) { 
+
+    const time = result.timer ?? 0; 
+
+    timerElement.textContent = `You have been on this page for ${time} seconds`;
+
+  }) 
+
+}
+updateTimer();
+setInterval(updateTimer, 1000);
 
 
 chrome.storage.sync.get("name", (data) => {
