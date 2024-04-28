fa9650febc023de0e84ca72e3c99218b20cc52ad
Deepak Nangia <dknangia@gmail.com>
2024-04-28 00:12:47 -0400

    TLS connection check

---
+++ Core31ConnectionTLS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Net;
+
+namespace Core31ConnectionTLS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                Console.WriteLine($"Application .NET framework : {System.Environment.Version.ToString()}");
+                //ServicePointManager.SecurityProtocol = SecurityProtocolType.SystemDefault;
+                Console.WriteLine($"Current Security Protocol :  {ServicePointManager.SecurityProtocol}");
+                var url = "https://www.howsmyssl.com/a/check";
+                var response = client.GetAsync(url).Result;
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine($"{url} {response.StatusCode} {content}");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
