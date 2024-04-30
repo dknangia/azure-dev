640954b6b307fa338d4fafb1dc1237c2ad51a4b8
Deepak Nangia <dknangia@gmail.com>
2024-04-29 22:10:04 -0400

    EF Testing

---
+++ EFTesting/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EFTesting.Models;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+var db = new PreqDbContext();
+var result = db.PreQuals.ToList();
+foreach (var preQual in result)
+{
+    Console.WriteLine(preQual.CatId);
+}
