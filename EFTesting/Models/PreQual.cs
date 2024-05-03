264252ad579434a245b400f2805092ef31813101
Deepak Nangia <dknangia@gmail.com>
2024-05-03 11:32:51 -0400

    PreQual

---
+++ EFTesting/Models/PreQual.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFTesting.Models;
+
+public partial class PreQual
+{
+    public string CatId { get; set; } = null!;
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public DateTime? CreatedTime { get; set; }
+
+    public DateTime? ModifiedTime { get; set; }
+}
