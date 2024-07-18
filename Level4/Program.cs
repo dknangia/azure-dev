1d9bb2a6895c36979d7522a54e968d58ffc1dbc9
Deepak Nangia <dknangia@gmail.com>
2024-07-17 22:10:28 -0400

    Silly program

--- Level4/Program.cs
+++ Level4/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string talkingAbout = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string size = Console.ReadLine();
+string c = "of Doom";
+string d = "3000";
+Console.WriteLine("The " + size + " " + talkingAbout + " of " + c + " " + d + "!");
+
+Console.ReadLine();
