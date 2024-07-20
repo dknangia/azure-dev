62bc3a7f65d7ed153424455da60489c050c33451
Deepak Nangia <dknangia@gmail.com>
2024-07-20 13:38:30 -0400

    Small math function

--- Player Guide/Level4/Program.cs
+++ Player Guide/Level4/Program.cs
@@ -1,5 +1,19 @@
 ﻿// Level 4 chapter
+Console.WriteLine("Why don't you enter the heigth of the Triangle");
 
-int result = (2 + 5) * 2;
+uint _height = 0;
+if (uint.TryParse(Console.ReadLine(), out uint heightResult))
+    _height = heightResult;
 
-Console.WriteLine(result);
+Console.WriteLine("Why dont you now enter the base of the triangle");
+
+uint _base = 0;
+if (uint.TryParse(Console.ReadLine(), out uint baseResult))
+    _base = baseResult;
+
+if (_height > 0 && _base > 0)
+{
+    Console.WriteLine(_height * _base / 2);
+}
+else
+    Console.WriteLine("Cannot calculate the area of the triangle");
