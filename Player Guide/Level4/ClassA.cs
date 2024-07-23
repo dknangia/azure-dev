882f12ce116704aa465e7cbb0c87b972f0abbe39
Deepak Nangia <dknangia@gmail.com>
2024-07-22 20:50:55 -0400

    Returning different DataType based on the provided argument in generic

---
+++ Player Guide/Level4/ClassA.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level4
+{
+    public class ClassA
+    {
+        public int Property1 { get; set; }
+    }
+
+    public class ClassB
+    {
+        public string Property1 { get; set; } = string.Empty;
+    }
+
+    public class ClassArgument
+    {
+
+    }
+
+    public class PgAdapger
+    {
+        public T GetT<T>(ClassArgument argument)
+        {
+            Type type = typeof(T);
+            if (type == typeof(ClassA))
+            {
+                return (T)Convert.ChangeType(new ClassA(), typeof(T));
+            }
+            else
+            {
+                return (T)Convert.ChangeType(new ClassB(), typeof(T));
+            }
+        }
+    }
+}
