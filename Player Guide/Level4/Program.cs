3e80f0d3c058d4b24e2332a332e1ffdac13e163c
Deepak Nangia <dknangia@gmail.com>
2024-07-24 23:29:47 -0400

    Small program to detect enemy

--- Player Guide/Level4/Program.cs
+++ Player Guide/Level4/Program.cs
@@ -1,1 +1,56 @@
-﻿
+﻿Console.WriteLine("Input X co-ordinates");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input Y co-oridinates");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string direction = string.Empty;
+string Message = $"Enemy is in {direction}";
+if (y > 0)
+{
+    if (x < 0)
+    {
+        direction = "NW";
+    }
+    else if (x == 0)
+    {
+        direction = "N";
+    }
+    else if (x > 0)
+    {
+        direction = "NE";
+    }
+}
+else if (y == 0)
+{
+    if (x < 0)
+    {
+        direction = "W";
+    }
+    else if (x == 0)
+    {
+        direction = "!";
+    }
+    else if (x > 0)
+    {
+        direction = "E";
+    }
+}
+else if (y < 0)
+{
+    if (x < 0)
+    {
+        direction = "SW";
+    }
+    else if (x == 0)
+        //{
+        direction = "S";
+}
+else if (x > 0)
+{
+    direction = "SE";
+}
+}
+
+
+Console.WriteLine(direction == "!" ? "Enemy is here" : Message);
