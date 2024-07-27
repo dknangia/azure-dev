91eacf9a725979e3fce97fdc9aee9868f7b7bf1f
Deepak Nangia <dknangia@gmail.com>
2024-07-26 23:10:26 -0400

    Checkin 1

---
+++ Player Guide/Level4/logicalConditions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level4
+{
+    internal class logicalConditions
+    {
+        public void Run()
+        {
+            Console.WriteLine("Input X co-ordinates");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input Y co-oridinates");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            string direction = string.Empty;
+            string Message = $"Enemy is in {direction}";
+            if (y > 0)
+            {
+                if (x < 0)
+                {
+                    direction = "NW";
+                }
+                else if (x == 0)
+                {
+                    direction = "N";
+                }
+                else if (x > 0)
+                {
+                    direction = "NE";
+                }
+            }
+            else if (y == 0)
+            {
+                if (x < 0)
+                {
+                    direction = "W";
+                }
+                else if (x == 0)
+                {
+                    direction = "!";
+                }
+                else if (x > 0)
+                {
+                    direction = "E";
+                }
+            }
+            else if (y < 0)
+            {
+                if (x < 0)
+                {
+                    direction = "SW";
+                }
+                else if (x == 0)
+                    //{
+                    direction = "S";
+            }
+            else if (x > 0)
+            {
+                direction = "SE";
+            }
+
+            Console.WriteLine(direction == "!" ? "Enemy is here" : Message);
+        }
+    }
+}
+
