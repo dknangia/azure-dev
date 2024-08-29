7ed6efc8074f10a0d0fbe49e38cf111080018389
Deepak Nangia <dknangia@gmail.com>
2024-08-29 17:52:03 -0400

    Local method

---
+++ Player Guide/Level4/LocalMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level4
+{
+    public class LocalMethods
+    {
+        public void WriteNumbers()
+        {
+            Console.WriteLine(Cube(2));
+            Console.WriteLine(Cube(3));
+            Console.WriteLine(Cube(4));
+
+            static int Cube(int n) => n * n * n;
+
+        }
+    }
+}
