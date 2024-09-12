d43772f08efe742d3fe4f57172254801ca588edf
Deepak Nangia <dknangia@gmail.com>
2024-09-12 19:28:40 -0400

    Third party deployment

--- Player Guide/Level4/Program.cs
+++ Player Guide/Level4/Program.cs
@@ -1,3 +1,35 @@
 ﻿using Level4;
+using Level4._3_Party_Deployment;
+using System.Reflection.Metadata.Ecma335;
 
-new SwitchesProgram().Run();
+//new SwitchesProgram().Run();
+
+
+//List<int> intList = new List<int>();
+//intList.Add(0);
+//intList.Add(1);
+    
+//intList.Add(2);
+//intList.Add(3);
+//intList.Add(4);
+//intList.Add(5);
+
+//static IEnumerable<int> FilterList(List<int> intList)
+//{
+//    foreach (int i in intList)
+//    {
+//        if(i > 4)
+//        {
+//            yield return i; // Basciall, we removed the need of any temporary list to store this comparsion result
+//        }
+//    }
+//}
+
+//foreach (int i in FilterList(intList))
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+new RunProgram(new MediaGroup());
