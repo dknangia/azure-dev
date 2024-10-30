14f29b7948364d4c9f62a6678723e3886886d82f
Deepak Nangia <dknangia@gmail.com>
2024-10-30 19:26:31 -0400

    Strategy Pattern

---
+++ DesignPatterns/Strategy Pattern/Strategy/EmailLog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Strategy_Pattern.Strategy
+{
+    public interface ILogging
+    {
+        public void DoLogging();
+    }
+
+
+    public class Context
+    {
+        public ILogging Logging;
+
+
+        public void DoLogging()
+        {
+            Logging.DoLogging();
+        }
+
+
+    }
+
+    public class EmailLogging : ILogging
+    {
+        public void DoLogging()
+        {
+            Console.WriteLine("Email logging");
+        }
+    }
+
+    public class DiskLogging : ILogging
+    {
+        public void DoLogging()
+        {
+            Console.WriteLine("Disk logging");
+        }
+    }
+}
