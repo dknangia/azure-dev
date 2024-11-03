e72cc31f7a656e63cdd35843fafeeb7e6b7480b2
Deepak Nangia <dknangia@gmail.com>
2024-11-03 16:02:41 -0500

    Search controller

---
+++ DesignPatterns/Asp.net MVC Patterns/ContactManager.V1/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using ContactManager.V1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.V1.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(string criteria, string searchBy)
+        {
+            var data = GetData(criteria, searchBy);
+            ViewBag.Criteria = criteria; 
+            ViewBag.SearchBy = searchBy;
+            return View(data);
+        }
+
+        public async Task<List<Customer>> GetData(string criteria, string searchBy)
+        {
+            var data = searchBy.ToLower() switch
+            {
+                "companyname" => await CustomerSearch.SearchByCompanyName(criteria),
+                "contactname" => await CustomerSearch.SearchByContactName(criteria),
+                "country" => await CustomerSearch.SearchByCountry(criteria),
+                _ => new List<Customer>()
+            };
+
+            return data;
+        }
+    }
+
+   
+}
