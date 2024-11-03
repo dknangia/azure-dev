7fc99ad112ba2542a33c6d3c993d4487c1516a38
Deepak Nangia <dknangia@gmail.com>
2024-11-02 21:59:37 -0400

    Contact model class

---
+++ DesignPatterns/Asp.net MVC Patterns/ContactManager.V1/Models/Model/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactManager.V1.Models.Model;
+
+[Table("Contacts")] // This should match with the table name in DB.
+public class Contact
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required] 
+    [StringLength(50)] 
+    public string FirstName { get; set; }
+
+    [Required] 
+    [StringLength(50)] 
+    public string LastName { get; set; }
+
+    [Required] 
+    [StringLength(50)] 
+    public string Email { get; set;  }
+    [Required] 
+    [StringLength(20)] 
+    public string Phone { get; set; }
+}
