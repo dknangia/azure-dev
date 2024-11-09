93dd4883dde01c687952831aba5b651bb8b18fe3
Deepak Nangia <dknangia@gmail.com>
2024-11-08 22:46:08 -0500

    Adding new Tax Calculator

---
+++ DesignPatterns/Asp.net MVC Patterns/ContactManager.V1/Models/Tax Calculator/ICountryTaxCalculator.cs
@@ -0,0 +1,43 @@
+﻿namespace ContactManager.V1.Models.Tax_Calculator
+{
+    public interface ICountryTaxCalculator
+    {
+        public decimal TotalIncome { get; set; }
+        public decimal TotalDeduction  { get; set; }
+
+        decimal CalculateTax();
+    }
+
+    public class TaxCalculateForUs : ICountryTaxCalculator
+    {
+        public decimal TotalIncome { get; set; }
+        public decimal TotalDeduction { get; set; }
+        public decimal CalculateTax()
+        {
+            decimal taxableIncome = TotalIncome - TotalDeduction;
+            return taxableIncome * 30 / 100;
+        }
+    }
+
+    public class TaxCalculateForIndia : ICountryTaxCalculator
+    {
+        public decimal TotalIncome { get; set; }
+        public decimal TotalDeduction { get; set; }
+        public decimal CalculateTax()
+        {
+            decimal taxableIncome = TotalIncome - TotalDeduction;
+            return taxableIncome * 35 / 100;
+        }
+    }
+
+    public class TaxCalculateForSpain : ICountryTaxCalculator
+    {
+        public decimal TotalIncome { get; set; }
+        public decimal TotalDeduction { get; set; }
+        public decimal CalculateTax()
+        {
+            decimal taxableIncome = TotalIncome - TotalDeduction;
+            return taxableIncome * 20 / 100;
+        }
+    }
+}
