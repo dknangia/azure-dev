e579c036f7b65b7476faf7b7b9e2b8220af4b6d8
Deepak Nangia <dknangia@gmail.com>
2024-11-15 21:43:10 -0500

    Singleton pattern

---
+++ DesignPatterns/Asp.net MVC Patterns/ContactManager.V1/Models/Creational/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ContactManager.V1.Models.Creational
+{
+    public class Singleton
+    {
+        private static Singleton? _instance;
+
+        private Singleton()
+        {
+
+        }
+        public static Singleton GetInstance()
+        {
+            return _instance ??= new Singleton();
+        }
+    }
+}
