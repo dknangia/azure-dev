1914acb25193dea2b5e3baafc9040b8d434e05ef
Deepak Nangia <dknangia@gmail.com>
2024-11-17 14:24:05 -0500

    Added IChart Provider

---
+++ DesignPatterns/Asp.net MVC Patterns/ContactManager.V1/Models/Creational/Factory/IChart.cs
@@ -0,0 +1,26 @@
+﻿namespace ContactManager.V1.Models.Creational.Factory
+{
+    public interface IChart
+    {
+        public string Title { get; set; }
+        public List<string> XData { get; set; }
+        public List<string> YData { get; set; }
+        public void GenerateChart();
+    }
+
+    public interface IChartProvider
+    {
+        public void GetChart();
+    }
+
+    public class BarChart : IChart
+    {
+        public string Title { get; set; }
+        public List<string> XData { get; set; }
+        public List<string> YData { get; set; }
+        public void GenerateChart()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
