632dc44ef2d18282bc68d0a64246aafa99331f9b
Deepak Nangia <dknangia@gmail.com>
2024-11-19 19:29:18 -0500

    Refeactoring the code

---
+++ DesignPatterns/Asp.net MVC Patterns/ContactManager.V1/Helper/ServiceHelper.cs
@@ -0,0 +1,14 @@
+﻿using ContactManager.V1.Contracts;
+
+namespace ContactManager.V1.Helper
+{
+    public static class ServiceHelper
+    {
+        public static IServiceCollection AddDependency(this IServiceCollection service)
+        {
+            service.AddScoped<IEmployee, Employee>();
+
+            return service;
+        }
+    }
+}
