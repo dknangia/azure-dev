8dff834e49d6663dbe18c0c43f7defc9fc28bdd3
Deepak Nangia <dknangia@gmail.com>
2024-11-19 19:21:51 -0500

    Testing _provider.GetService() functionality

---
+++ DesignPatterns/Asp.net MVC Patterns/ContactManager.V1/Contracts/IEmployee.cs
@@ -0,0 +1,12 @@
+﻿namespace ContactManager.V1.Contracts
+{
+    public interface IEmployee
+    {
+        string Name { get; }
+    }
+
+    public class Employee : IEmployee
+    {
+        public string Name { get; }
+    }
+}
