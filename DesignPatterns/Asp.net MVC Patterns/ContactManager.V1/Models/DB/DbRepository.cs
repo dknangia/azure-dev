2322c05c4b0f551037fc648e24e6f88fc33b1552
Deepak Nangia <dknangia@gmail.com>
2024-11-22 18:58:33 -0500

    DB repository

---
+++ DesignPatterns/Asp.net MVC Patterns/ContactManager.V1/Models/DB/DbRepository.cs
@@ -0,0 +1,56 @@
+﻿namespace ContactManager.V1.Models.DB
+{
+    public class DbRepository
+    {
+        public void GetData<T>(Func<Person, List<T>> extract)
+        {
+            Person p = new Person
+            {
+                Name = "George",
+                SurName = "Walmart"
+            };
+
+            extract(p);
+        }
+    }
+
+    public class Caller
+    {
+        public void GetData()
+        {
+            List<Employee> ExtractEmployee(Person p)
+            {
+                var result = new List<Employee> { new() { FirstName = p.Name, LastName = p.SurName } };
+                return result;
+            }
+
+            var x = new DbRepository();
+            x.GetData(ExtractEmployee);
+        }
+
+        //private List<Employee> Extract(Person? p)
+        //{
+        //    var result = new List<Employee>
+        //    {
+        //        new()
+        //        {
+        //            FirstName = p.Name,
+        //            LastName = p.SurName
+        //        }
+        //    };
+        //    return result;
+        //}
+    }
+
+    public class Employee
+    {
+        public  string LastName { get; set; }   
+        public string FirstName { get; set; }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+    }
+}
